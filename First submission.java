9dedde012102ae27c53d925eef45f4280e5b2478
MrMalasada <riley.malasig@gmail.com>
2025-08-13 08:34:27 -1000

    Create First submission.java

---
+++ First submission.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+
+// Task one: This line of code prints out "Hello World"    
+    System.out.print("Hello World");
+
+/*  Task two: This line of code prints out "Hello World" 
+and skips to the next line
+*/
+    System.out.println("Hello World");
+    
+/* Task two: This line of code prints out "Hello World" on 
+a new line under the first "Hello World" 
+*/
+    System.out.print("Hello World");
+    
+  }
+}
