2a6b99359231c0fa7ae8a78f9d68e837f39cde02
MrMalasada <riley.malasig@gmail.com>
2025-10-02 12:54:00 -1000

    Create Parallelogram.java

---
+++ ShapesProject/Parallelogram.java
@@ -0,0 +1,63 @@
+package shapes;
+/**
+ * this is the class where I make parallelogram objects
+ */
+public class Parallelogram {
+	
+	/*
+	 * i make three private null doubles for the long side, 
+	 * short side and the height
+	 */
+	private double longSide;
+	
+	private double shortSide;
+	
+	private double height;
+	
+	/*
+	 * in this constructor i give the parallelogram object a
+	 * long side, short side and a height
+	 */
+	public Parallelogram (double l, double s, double h)
+	{
+		longSide = l;
+		
+		shortSide = s;
+		
+		height = h;
+	}
+	
+	/*
+	 * this calculates and returns the area of a parallelogram
+	 * object
+	 */
+	public double getArea()
+	{
+		return longSide*height;
+	}
+	
+	/*
+	 * this calculates and returns the height of a parallelogram
+	 * object
+	 */
+	public double getHeight()
+	{
+		return (longSide*height)/longSide;
+	}
+	
+	/*
+	 * this calculates and returns the
+	 * perimeter of a parallelogram object
+	 */
+	public double getPerimeter()
+	{
+		return 2*(shortSide+longSide);
+	}
+	
+	//this prints the sum of all interior angles of a parallelogram object
+		public static void getSumOfInteriorAngles()
+		{
+			System.out.print("The sum of all of the interior angles of a parallelogram is 360 degrees");
+		}
+	
+}
