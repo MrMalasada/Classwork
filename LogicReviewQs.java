c54a6d7a10664add8059fd29faaf82e84e3f5e42
MrMalasada <riley.malasig@gmail.com>
2025-11-03 14:50:04 -1000

    Create LogicReviewQs.java

---
+++ LogicReviewQs.java
@@ -0,0 +1,31 @@
+/*
+* 1. A boolean is a primitive data type that holds true or false
+*
+* 2. this code will only run when test == true
+*
+* 3. that code will only run when test == false
+*
+* 4a. The difference between the two code statements is that in
+* A, "other code stuffs" will only run when test == false and 
+* code stuffs and other code stuffs will never run at the same time.
+*
+* In B, code stuffs and other code stuffs are in independent
+* if statements. Code stuffs will only run when test == true, and other code
+* stuffs will only run when test2 == true. In this case, it is possible for
+* code stuffs and other code stuffs to run at the same time.
+*
+* 4b. In case A either code stuffs and other code stuffs are garunteed to be
+* ran as long as test is being ran.
+* 
+*
+* In case B code is not garrunteed to be ran, if test and test2 
+* are both false, then no code will run at all
+*
+* 5. code3 will never run because test is a boolean, true or false,
+* since there are already cases for test being true and false, code3 is unreachable code
+*
+* 6. code3 will only run when test and test2 are both false
+*
+* 7.
+*
+*/
