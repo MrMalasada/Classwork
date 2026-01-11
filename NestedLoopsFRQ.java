eb0322c6639a298993a6ccb577990c452c3e4d91
MrMalasada <riley.malasig@gmail.com>
2026-01-11 12:29:11 -1000

    Update NestedLoopsFRQ.java

--- NestedLoopsFRQ.java
+++ NestedLoopsFRQ.java
@@ -1,32 +1,50 @@
-public static void randomizeUntilNum(int num, int iter)
-  {
-    int randNum = (int) (Math.random * 10) + 1);
 
-    boolean printedNum = false;
+public class NestedIterationFrq {
 
-    while(iter>0)
-      {
-        while(!printedNum)
-          {
-            if(randNum == num)
-            {
-              System.out.print(randNum);
+	public static void main(String[] args) {
+		
+		randomizeUntilNum(7,6);
 
-              printedNum = true;
-            }
-            else if(randNum != num)
-            {
-              System.out.print(randNum);
+	}
+	
+	public static void randomizeUntilNum(int num, int iter)
+	  {
+	    int randNum = (int) ((Math.random() * 9) + 1);
 
-              printedNum = true;
-            }
-          }
+	    boolean printedNum = false;
 
-        System.out.println("");
+	    while(iter>0)
+	      {
+	        while(!printedNum)
+	          {
+	            if(randNum == num)
+	            {
+	              System.out.print(randNum);
+	              
+	              randNum = (int) ((Math.random() * 10) + 1);
 
-        iter --;
-      }
+	              printedNum = true;
+	            }
+	            else if(randNum != num)
+	            {
+	              System.out.print(randNum);
+	              
+	              randNum = (int) ((Math.random() * 10) + 1);
+	              
+
+	              printedNum = false;
+	            }
+	          }
+
+	        System.out.println("");
+
+	        iter --;
+	        
+	        printedNum = false;
+	      }
+	}
+	          
+
+	      
+
 }
-          
-
-      
