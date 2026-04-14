d014dfed65aa3f8440ed335b2a35f8bffb4ff580
MrMalasada <riley.malasig@gmail.com>
2026-04-14 13:58:42 -1000

    Update recursion1CodingBat.java

--- recursion/recursion1CodingBat.java
+++ recursion/recursion1CodingBat.java
@@ -25,3 +25,18 @@ public int bunnyEars(int bunnies) {
 }
 
 //fibonacci
+
+public int fibonacci(int n) {
+  if(n == 0)
+  {
+    return 0;
+  }
+  else if (n == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+}
